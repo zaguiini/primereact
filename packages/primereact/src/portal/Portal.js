f656c5b9fe7d1358261e0c7ad5480fbaf4913a56
Mert Sincan <sincan.mert@gmail.com>
2024-09-11 14:37:53 +0100

    Update Portal component

--- packages/primereact/src/portal/Portal.js
+++ packages/primereact/src/portal/Portal.js
@@ -1,44 +1,40 @@
+import { ComponentProvider } from '@primereact/core/component';
+import { PrimeReactContext } from '@primereact/core/config';
 import { useMountEffect, useUnmountEffect, useUpdateEffect } from '@primereact/hooks';
+import { isClient, resolve } from '@primeuix/utils';
 import * as React from 'react';
-import ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom';
+import { usePortal } from './Portal.base';
 
 export const Portal = React.memo((inProps) => {
-    //const context = React.useContext(PrimeReactContext);
-
-    const portal = useInputText(inProps);
+    const config = React.useContext(PrimeReactContext);
+    const portal = usePortal(inProps);
     const { props } = portal;
 
-    const [mountedState, setMountedState] = React.useState(props.visible && DomHandler.isClient());
+    const [mountedState, setMountedState] = React.useState(props.visible && isClient());
 
     useMountEffect(() => {
-        if (DomHandler.isClient() && !mountedState) {
+        if (isClient() && !mountedState) {
             setMountedState(true);
-            props.onMounted && props.onMounted();
+            props.onMounted?.();
         }
     });
 
     useUpdateEffect(() => {
-        props.onMounted && props.onMounted();
+        props.onMounted?.();
     }, [mountedState]);
 
     useUnmountEffect(() => {
-        props.onUnmounted && props.onUnmounted();
+        props.onUnmounted?.();
     });
 
     const element = props.element || props.children;
 
     if (element && mountedState) {
-        let appendTo = props.appendTo || (context && context.appendTo) || PrimeReact.appendTo;
+        const appendTo = resolve(props.appendTo || config?.appendTo) || document.body;
+        const content = appendTo === 'self' ? element : ReactDOM.createPortal(element, appendTo);
 
-        if (ObjectUtils.isFunction(appendTo)) {
-            appendTo = appendTo();
-        }
-
-        if (!appendTo) {
-            appendTo = document.body;
-        }
-
-        return appendTo === 'self' ? element : ReactDOM.createPortal(element, appendTo);
+        return <ComponentProvider value={portal}>{content}</ComponentProvider>;
     }
 
     return null;
