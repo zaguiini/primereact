09b70e1679218c6d90916b25903d7bc39595c3b0
Mert Sincan <sincan.mert@gmail.com>
2024-09-17 18:18:40 +0100

    Update Component.js

--- packages/core/src/component/Component.js
+++ packages/core/src/component/Component.js
@@ -1,10 +1,40 @@
-import { resolve } from '@primeuix/utils';
+import { isArray, isObject, resolve } from '@primeuix/utils';
 import * as React from 'react';
 import { ComponentContext } from './Component.context';
 
+// @todo: add omit(inProps, ['pIf']);
 export const Component = React.forwardRef((inProps, ref) => {
-    const { is = React.Fragment, render, children, ...rest } = inProps || {};
-    const context = React.useContext(ComponentContext);
+    if (inProps.pIf === false) return null;
 
-    return render ? resolve(render, context) : React.createElement(is, { ref, ...rest }, resolve(children, context));
+    const context = React.useContext(ComponentContext);
+    const { as, pIf, instance = context, children, options, ...rest } = inProps || {};
+
+    return as ? React.createElement(as, { ref, ...rest }, resolve(children, { instance, ...rest, ...options })) : null; // @todo: check params
+});
+
+export const Slot = React.forwardRef((inProps, ref) => {
+    if (inProps.pIf === false) return null;
+
+    const context = React.useContext(ComponentContext);
+    const { name, pIf, instance = context, children, render, ...rest } = inProps || {};
+    const content = resolve(instance?.$slots?.[name] || instance?.props?.[name], { instance, default: children, ...rest });
+
+    //return resolve(instance?.$slots?.[name] || instance?.props?.[name], { instance, default: children, ...rest }) ?? children;
+    return resolve(render, { instance, content, default: children, ...rest }) ?? content ?? children;
+});
+
+export const Template = React.forwardRef((inProps, ref) => {
+    if (inProps.pIf === false) return null;
+
+    const context = React.useContext(ComponentContext) || {};
+    const { slot, pFor, instance = context || {}, children } = inProps || {};
+
+    if (slot) {
+        instance.$slots ||= {};
+        instance.$slots[slot] = children;
+
+        return null;
+    }
+
+    return isArray(pFor) ? pFor.map((value, index) => resolve(children, { value, index })) : isObject(pFor) ? Object.entries(pFor).map(([key, value], index) => resolve(children, { key, value, index })) : resolve(children, { instance });
 });
